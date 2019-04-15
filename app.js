8c92c3166def6cbe3320397a06c0a36fa9cda2fa
Emanuele <emanuelegorga@gmail.com>
2019-04-15 18:55:02 +0100

    added bootstrap and initial partials

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("This will be soon the landing page!");
+  res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+  let campgrounds = [
+    {
+      name: "JavaScript",
+      image:
+        "https://image.freepik.com/free-icon/js-rounded-square_318-10133.jpg"
+    },
+    {
+      name: "Node",
+      image:
+        "https://ih1.redbubble.net/image.109336634.1604/flat,550x550,075,f.u1.jpg"
+    },
+    {
+      name: "React",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4AuEQCCvVDUk_LhSstNzOBzrxvUcExKex6nZUgBMdeLyTNzr3"
+    }
+  ];
+
+  res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.listen(3000, function() {
