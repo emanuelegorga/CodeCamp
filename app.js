2d3f20bd40ceef3cb550e18c208dac671535cc35
Emanuele <emanuelegorga@gmail.com>
2019-04-20 12:40:28 +0100

    styled campground image container

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const   express    = require("express"),
 mongoose.connect("mongodb://localhost:27017/codecamp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static("./public"))
 seedDB();
 
 app.get("/", (req, res) => {
