0bc2b557ca58d36eaf74c03e20f96649d6747ae4
Emanuele <emanuelegorga@gmail.com>
2019-04-22 18:12:41 +0100

    added login logic up and running

--- app.js
+++ app.js
@@ -132,6 +132,19 @@ app.post("/register", (req, res) => {
     });
 })
 
+// show login form
+app.get("/login", (req,res) => {
+    res.render("login");
+})
+
+// login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), (req, res) => {
+});
+
 app.listen(3000, function() {
   console.log("The CodeCamp Server Has Started!");
 });
