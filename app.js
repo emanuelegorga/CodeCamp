55ca2fe52222460bd17b2a18eadb05212bfa1f17
Emanuele <emanuelegorga@gmail.com>
2019-04-22 17:33:58 +0100

    setting up PASSPORT within app.js file

--- app.js
+++ app.js
@@ -13,8 +13,20 @@ mongoose.connect("mongodb://localhost:27017/codecamp", { useNewUrlParser: true }
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-// app.use(express.static("./public"))
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Codecamp is a great project",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", (req, res) => {
   res.render("landing");
