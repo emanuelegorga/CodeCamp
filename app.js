35ff665652b87d9d64c2d5d90c73764c327efa1f
Emanuele <emanuelegorga@gmail.com>
2019-04-22 17:29:53 +0100

    created user model

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const   express    = require("express"),
-        app        = express(),
-        bodyParser = require("body-parser"),
-        mongoose   = require("mongoose"),
-        Campground = require("./models/campground"),
-        Comment    = require("./models/comment"), 
-        seedDB     = require("./seeds");
+const   express       = require("express"),
+        app           = express(),
+        bodyParser    = require("body-parser"),
+        mongoose      = require("mongoose"),
+        passport      = require("passport"),
+        LocalStrategy = require("passport-local"),
+        Campground    = require("./models/campground"),
+        Comment       = require("./models/comment"),
+        User          = require("./models/user"),
+        seedDB        = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/codecamp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
