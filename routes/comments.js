0d475a8ca10506a0d70d7ff633242478eda181f3
Emanuele <emanuelegorga@gmail.com>
2019-06-08 12:25:31 +0100

    Edit Comment feature
    * Added a button to edit comments
    * Added functionality to edit comments

--- routes/comments.js
+++ routes/comments.js
@@ -40,6 +40,28 @@ router.post("/", isLoggedIn, (req, res) => {
     })
 });
 
+// Comments edit
+router.get("/:comment_id/edit", (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+        } 
+    })
+})
+
+// Comment Update
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if(err) {
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
+        }
+    })
+})
+
 //middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
