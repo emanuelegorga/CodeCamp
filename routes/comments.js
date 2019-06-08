031978d80d4f59fe7e496755a0e9eee4e78bc520
Emanuele <emanuelegorga@gmail.com>
2019-06-08 14:26:02 +0100

    Comments can be deleted only if logged in
    * Edit and Delete buttons can be seen only if the user is the owner of the coment
    * Comment can be edited and delited only if owning these comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,7 +41,7 @@ router.post("/", isLoggedIn, (req, res) => {
 });
 
 // Comments edit
-router.get("/:comment_id/edit", (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if(err) {
             res.redirect("back");
@@ -63,7 +63,7 @@ router.put("/:comment_id", (req, res) => {
 })
 
 // Comment Destroy
-router.delete("/:comment_id", (req, res) => {
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
     Comment.findByIdAndRemove(req.params.comment_id, (err) => {
         if(err) {
             res.redirect("back")
@@ -79,6 +79,30 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    // is user logged in
+    if (req.isAuthenticated()) {
+        // find the correct campground
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err) {
+                res.redirect("back");
+            } else {
+                // does the user own the comment?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    // otherwise, redirect
+                    res.redirect("back");
+                }
+            }
+        })
+    } else {
+        //if not, redirect
+        console.log("You need to be logged in!")
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
