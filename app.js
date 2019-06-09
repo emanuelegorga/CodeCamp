2f8bebde895d88039d982f0a9ba15672bfac2701
Emanuele <emanuelegorga@gmail.com>
2019-06-09 12:32:27 +0100

    Deploying db commit

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const commentRoutes     = require("./routes/comments"),
       campgroundRoutes  = require("./routes/campgrounds"),
       indexRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/codecamp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/codecamp", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://gorgaemanuele:samplepassw@codecamp-xqmyl.mongodb.net/test?retryWrites=true&w=majority", { 
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => {
+  console.log('Connected to DB');
+}).catch(err => {
+  console.log('Error: ', err.message)
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
